5c03c5628511a0c8c3069315b42d54bf7e350ae2
busuyoc <busuioc.p.claudiu@gmail.com>
2025-04-10 01:15:52 +0300

    Center content, add padding all around

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,19 @@
+type Props = { // defines a type in TS
+    children?: ReactNode, // ? marks the member as being optional
+}
+
+export default function LoggedOutLayout({ children }: Props) {
+    // in next, any layout files in any dirs will have automatically passed {children}
+    // which represents the page inside that layout
+    
+    // landing page is wrapped inside /app/layout.tsx which is wrapped inside (logged-out)/layout.tsx, which is wrapped inside out (logged-out) group
+    
+    return (
+        <div className="flex flex-col min-h-screen items-center justify-center p-24">
+            {/* children have flex, stacked vertically (in col)*/}
+            {/* body has min-height: 100vh, padding: 96px */}
+            {/* items are justified (centered on the main axis - [col]) */}
+            {children}
+        </div>
+    )
+}
--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,4 +1,15 @@
 import { Button } from "@/components/ui/button"
+{ /* Next.js has in-built support for the "paths" and "baseUrl" options of tsconfig.json and jsconfig.json files.
+
+These options allow you to alias project directories to absolute paths, making it easier to import modules. For example:
+
+// before
+import { Button } from '../../../components/button'
+ 
+// after
+import { Button } from '@/components/button'
+
+https://nextjs.org/docs/13/app/building-your-application/configuring/absolute-imports-and-module-aliases*/}
 export default function LandingPage() {
     return (
         <div>
