03bffa2cb2dd61d6bbd188d21373b07c570864a8
busuyoc <busuioc.p.claudiu@gmail.com>
2025-04-10 02:46:25 +0300

    Define styles for h1-h6 Added <Link> Next component w routes Add styles for layout Add autoprefixer & postcss for no reason (look into that)

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,5 +1,5 @@
 type Props = { // defines a type in TS
-    children?: ReactNode, // ? marks the member as being optional
+    children?: ReactNode, // ? marks the member as being optional in TS
 }
 
 export default function LoggedOutLayout({ children }: Props) {
@@ -9,7 +9,7 @@ export default function LoggedOutLayout({ children }: Props) {
     // landing page is wrapped inside /app/layout.tsx which is wrapped inside (logged-out)/layout.tsx, which is wrapped inside out (logged-out) group
     
     return (
-        <div className="flex flex-col min-h-screen items-center justify-center p-24">
+        <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
             {/* children have flex, stacked vertically (in col)*/}
             {/* body has min-height: 100vh, padding: 96px */}
             {/* items are justified (centered on the main axis - [col]) */}
