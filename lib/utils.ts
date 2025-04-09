88f7151a90f0076fe6a8a1afdcc3a01fafa353f2
busuyoc <busuioc.p.claudiu@gmail.com>
2025-04-10 00:29:16 +0300

    Initialize project structure Added shadcn button

---
+++ lib/utils.ts
@@ -0,0 +1,6 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) { // cn() concatenates a bunch class names ???
+  return twMerge(clsx(inputs)) // tailwind merge makes sure we don't have conflicting classes
+}
