5e0a474dd098d706a9c5d8aa1ccc246954939035
busuyoc <busuioc.p.claudiu@gmail.com>
2025-04-10 15:03:48 +0300

    Install ThemeProvider to switch between light/dark depending on user pref

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import {cn} from "../lib/utils"
+import { cn } from "../lib/utils";
+import { ThemeProvider } from "@/components/theme.provider";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,15 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={cn(poppins.className, "dark")}
+        className={cn(`${poppins.className} antialised`)} // removed dark since Ill implement theme switch
       >  {/* {`${poppins.className} antialiased`} comments need to be in JSv
             - the above was before
             - now using the cn() fn to concat the css class names
             - "dark" comes from tailwind*/
-            }
-        {children}
+        }
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
